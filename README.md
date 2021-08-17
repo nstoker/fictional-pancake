f4cead4c8fb2a0ed2d0a7144ec9767a9a3ad41f1
Neil <neil.stoker@clixifix.co.uk>
2021-08-17 11:18:57 +0100

    Initial golang Vue server

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 A Go server for a Vue SPA.
 
 Initally following [How I set up a real world project with Go and Vue](https://www.freecodecamp.org/news/how-i-set-up-a-real-world-project-with-go-and-vue/).
+
+## Environment variables
+
+Either a 'proper' environment variable can be set, or add to the `.env` file. Copy `.example.env` to get default values.
+
+`FRONTEND` is the location the front end will be served from. Defaults to `./frontend/dist`.
+`PORT` is the port the server will listen for connections on. Default `3030`.
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/nstoker/fictional-pancake/internal/handlers"
 	"github.com/nstoker/fictional-pancake/internal/version"
 	"github.com/sirupsen/logrus"
@@ -12,13 +14,33 @@ import (
 func main() {
 	logrus.Infof("Hello, world! %s starting", version.Version())
 
-	port := os.Getenv("PORT")
+	err := godotenv.Load(".env")
+	if err != nil {
+		logrus.Infof("error loading godotenv file %v", err)
+	}
 
-	http.HandleFunc("/", handlers.HomePageHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3030"
+		logrus.Infof("environment variable PORT not set, defaulting to %s", port)
+	}
+
+	frontend := os.Getenv("FRONTEND")
+	if frontend == "" {
+		frontend = "./frontend/dist"
+		logrus.Infof("environment variable FRONTEND not set, defaulting to %s", frontend)
+	}
+
+	logrus.Infof("serving spa from %s", frontend)
+
+	http.HandleFunc("/hello", handlers.HomePageHandler)
+
+	fs := http.FileServer(http.Dir(frontend))
+	http.Handle("/", fs)
 
 	logrus.Infof("Listening on %s", port)
 
 	logrus.Panic(
-		http.ListenAndServe(":3000", nil),
+		http.ListenAndServe(fmt.Sprintf(":%s", port), nil),
 	)
 }
